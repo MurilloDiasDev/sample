fa37f27394553ce37fac70a94d323344644ac4e3
Murillo Dias <murillao2007@gmail.com>
2023-05-31 01:27:03 -0300

    Site Home Laboral no novo servidor

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-
-	$('[data-bs-chart]').each(function(index, elem) {
-		this.chart = new Chart($(elem), $(elem).data('bs-chart'));
-	});
-
+	AOS.init({ disable: 'mobile' });
 });
