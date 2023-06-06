0326bc9e38c4f821e2d826e0d1b132c264ada14b
Murillo Dias <murillao2007@gmail.com>
2023-06-05 22:53:23 -0300

    teste table

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
+
+	$('[data-bs-chart]').each(function(index, elem) {
+		this.chart = new Chart($(elem), $(elem).data('bs-chart'));
+	});
+
 });
